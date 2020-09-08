7819baa441ebfd645de64b921d7e844f84a52d9e
Shambonik <shambonik@gmail.com>
2020-09-08 22:38:30 +0300

    practice_3_2-Kuzin

---
+++ practice_3-Kuzin/practice_3_2-Kuzin/src/com/shambonik/Square.java
@@ -0,0 +1,46 @@
+package com.shambonik;
+
+public class Square extends Rectangle {
+    public Square() {
+        super();
+    }
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    public Square(String color, boolean filled, double side) {
+        super(color, filled, side, side);
+    }
+
+    public void setSide(double side){
+        this.length = side;
+        this.width = side;
+    }
+
+    public double getSide(){
+        return length;
+    }
+
+    @Override
+    public void setWidth(double side) {
+        super.setWidth(side);
+        super.setLength(side);
+    }
+
+    @Override
+    public void setLength(double side) {
+        super.setLength(side);
+        super.setWidth(side);
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "width=" + width +
+                ", length=" + length +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
